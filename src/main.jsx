a86815f69b177b3eed0f7e3924971c8dcebe1477
unknown <amayabandara0604@gmail.com>
2024-07-16 13:23:50 +0530

    use Java

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Make sure this file includes Tailwind CSS imports
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
